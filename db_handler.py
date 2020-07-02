8d8e500eb53046d8d343ff3a78d99a0b1706596b
Gajesh Bhat <gajeshbht@gmail.com>
2020-07-02 15:45:10 -0400

    Refactoring 1

--- db_handler.py
+++ db_handler.py
@@ -1,5 +1,8 @@
 import logging
+import traceback
+
 from pymongo import MongoClient
+
 
 class DBHandlers():
     """Handles database methods for the crawler"""
@@ -9,7 +12,8 @@ class DBHandlers():
         self.DB_URI_STRING = "mongodb://localhost:27017/"
         logging.basicConfig(filename='db_logs.log', level=logging.INFO)
 
-    def log_message(self,message):
+    @staticmethod
+    def log_message(message):
         logging.info(msg=message)
 
     def get_db_new_instance(self):
@@ -17,27 +21,24 @@ class DBHandlers():
         dev_info = db_client.dev_info
         return dev_info
 
-    def insert_item(self,cursor,item):
+    def insert_item(self, cursor, item):
         try:
             cursor.insert_one(item)
         except Exception as e:
             self.log_message("Error in insert_item()" + "\n" + str(traceback.format_exc()))
 
-    def delete_collection(self,cursor):
+    def delete_collection(self, cursor):
         try:
             cursor.remove()
         except Exception as e:
             self.log_message("Error in remove() on a collection" + "\n" + str(traceback.format_exc()))
 
-    def insert_item_github(self,item):
+    def insert_item_github(self, item):
         db_client = self.get_db_new_instance()
         github_cursor = db_client.github_info
-        self.insert_item(github_cursor,item)
+        self.insert_item(github_cursor, item)
 
     def delete_github_collection(self):
         db_client = self.get_db_new_instance()
         github_cursor = db_client.github_info
         self.delete_collection(github_cursor)
-
-if __name__ == '__main__':
-    main()
